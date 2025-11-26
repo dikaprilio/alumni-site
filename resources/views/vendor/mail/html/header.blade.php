20f189c7b5e00fdcbc1080276a72c95affa756eb
dikaprilio <apridika07@gmail.com>
2025-11-26 16:59:02 +0700

    email fix

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,11 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block; text-decoration: none; color: #1f2937;">
+<a href="{{ $url }}" style="display: inline-block; text-decoration: none; color: #ffffff;">
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-<span style="font-size: 24px; font-weight: bold; color: #1f2937;">{!! $slot !!}</span>
+<span style="font-size: 26px; font-weight: 700; color: #ffffff; letter-spacing: -0.02em;">{!! $slot !!}</span>
 @endif
 </a>
 </td>
